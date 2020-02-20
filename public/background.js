0aab179cf7c34343205a5e3a0fc7d45615dd2f8a
Matthew Bunday <mkbunday@gmail.com>
2020-02-20 14:05:03 -0500

    Fix async

--- public/background.js
+++ public/background.js
@@ -1,9 +1,10 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("Received message!!!");
-  return fetch("https://fathomless-beyond-31330.herokuapp.com/api/profiles")
+  fetch("https://fathomless-beyond-31330.herokuapp.com/api/profiles")
     .then(resp => resp.json())
     .then(res => {
       console.log(res);
       return sendResponse(res);
     });
+  return true;
 });
