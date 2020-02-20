8ae2e2a1982e8262f5fbd046f9013059d82afa4d
Julian Ramírez <julianramirez@gmail.com>
2020-02-20 15:56:05 -0500

    msg

--- public/background.js
+++ public/background.js
@@ -1,9 +1,19 @@
 const baseUrl = "https://fathomless-beyond-31330.herokuapp.com/api";
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+  console.log("REQUEST: " + request)
+
   if (request.requestId === "EditProfile") {
-    console.log("YEAHHH");
     sendResponse("PROFILE EDITED");
+  } else if (request.requestId === "GetProfile") {
+    console.log('FETCHING: ' + request.params.id)
+      fetch(baseUrl + '/profiles/' + request.params.id)
+        .then(resp => resp.json())
+        .then(res => {
+          console.log(res);
+          return sendResponse(res);
+        });    
   } else if (request.requestId === "ListMembers") {
     fetch("https://fathomless-beyond-31330.herokuapp.com/api/profiles")
       .then(resp => resp.json())
--- src/EditProfile.js
+++ src/EditProfile.js
@@ -3,21 +3,67 @@ import React, { Component } from "react"
 
 export default class EditProfile extends Component { 
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            tagline: '',
+            relationship_status: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
     componentDidMount() {
-        chrome.runtime.sendMessage({ requestId: "EditStatus", params: { id: "1" }, payload: { name: "NewName" } }, response => {
-          console.log(response);
-        });
-      }
-
-
-    render() {
         const parts = document.querySelector(".feed-identity-module__actor-meta")
         .querySelector("a")
         .getAttribute("href").split("/");
+        const userId = parts.pop() || parts.pop();
 
-        var userId = parts.pop() || parts.pop();
+        chrome.runtime.sendMessage({ requestId: "GetProfile", params: { id: "1" } }, response => {
+            console.log("RESPONSE: " +  response);
+            if (response) {
+                this.setState({ loading: false, tagline: response.tagline, relationship_status: response.relationship_status });
+            } else {
+                this.setState({ loading: false, tagline: '', relationship_status: '' });
+            }           
+        });
 
+    }
 
-        return (<div>userId</div>);
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        // chrome.runtime.sendMessage({ requestId: "EditStatus", params: { id: "1" }, payload: { name: "NewName" } }, response => {
+        //     console.log(response);
+        //   });
+
+        // alert('You info has been updated');
+        event.preventDefault();
+    }
+
+    render() {
+
+        if (this.state.loading) {
+            return (<div>Loading...</div>);
+        } else {
+            return (<div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Tagline:
+                        <input type="text" name="tagline" value={this.state.tagline} onChange={this.handleChange} />
+                    </label>
+    
+                    <label>
+                        Relationship Status:
+                        <input type="text" name="relationship_status" value={this.state.relationship_status} onChange={this.handleChange} />
+                    </label>      
+                    <input type="submit" value="Submit" />                  
+                </form>
+            </div>);
+        }        
     }
 }
