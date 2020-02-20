3f7401dda39b4a90575e83d0fcdb915d45f3c574
Matthew Bunday <mkbunday@gmail.com>
2020-02-20 14:13:08 -0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import MemberList from "./MemberList";
+import EditProfile from "./EditProfile";
 
-class App extends Component {
-  clickMe = () => {
-    chrome.runtime.sendMessage({ greeting: "hello" }, function(response) {
-      console.log(response);
-    });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to LinkedUp</h2>
-        </div>
-        <p className="App-intro">
-          See the following LinkedIn members who have opted-in to dating.
-        </p>
-        <button onClick={this.clickMe}>Click me</button>
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to LinkedUp</h2>
       </div>
-    );
-  }
-}
+      <MemberList />
+      <EditProfile />
+    </div>
+  );
+};
 
 export default App;
