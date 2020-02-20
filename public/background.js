3faa0e3264f87b833b648facf364335c298153ba
Matthew Bunday <mkbunday@gmail.com>
2020-02-20 16:13:53 -0500

    POST and PATCH?

--- public/background.js
+++ public/background.js
@@ -1,21 +1,33 @@
 const baseUrl = "https://fathomless-beyond-31330.herokuapp.com/api";
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-  console.log("REQUEST: " + request)
+  console.log("REQUEST: " + request);
 
   if (request.requestId === "EditProfile") {
     sendResponse("PROFILE EDITED");
+    if (request.params.profileExists) {
+      // PATCH request
+      fetch(`${baseUrl}/profile/${request.params.id}`, {
+        method: "PATCH",
+        body: JSON.stringify(request.payload)
+      });
+    } else {
+      // Create new
+      fetch(`${baseUrl}/profiles/`, {
+        method: "POST",
+        body: JSON.stringify(request.payload)
+      });
+    }
   } else if (request.requestId === "GetProfile") {
-    console.log('FETCHING: ' + request.params.id)
-      fetch(baseUrl + '/profiles/' + request.params.id)
-        .then(resp => resp.json())
-        .then(res => {
-          console.log(res);
-          return sendResponse(res);
-        });    
+    console.log("FETCHING: " + request.params.id);
+    fetch(`${baseUrl}/profiles/${request.params.id}`)
+      .then(resp => resp.json())
+      .then(res => {
+        console.log(res);
+        return sendResponse(res);
+      });
   } else if (request.requestId === "ListMembers") {
-    fetch("https://fathomless-beyond-31330.herokuapp.com/api/profiles")
+    fetch(`${baseUrl}/profiles`)
       .then(resp => resp.json())
       .then(res => {
         console.log(res);
--- src/EditProfile.js
+++ src/EditProfile.js
@@ -1,69 +1,108 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
+export default class EditProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      tagline: "",
+      relationship_status: "",
+      profileExists: null
+    };
+  }
 
-export default class EditProfile extends Component { 
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            tagline: '',
-            relationship_status: ''
-        };
+  componentDidMount() {
+    const parts = document
+      .querySelector(".feed-identity-module__actor-meta")
+      .querySelector("a")
+      .getAttribute("href")
+      .split("/");
+    const userId = parts.pop() || parts.pop();
+    const name = document.querySelector(
+      '[data-control-name="identity_welcome_message"]'
+    ).innerText;
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    componentDidMount() {
-        const parts = document.querySelector(".feed-identity-module__actor-meta")
-        .querySelector("a")
-        .getAttribute("href").split("/");
-        const userId = parts.pop() || parts.pop();
-
-        chrome.runtime.sendMessage({ requestId: "GetProfile", params: { id: "1" } }, response => {
-            console.log("RESPONSE: " +  response);
-            if (response) {
-                this.setState({ loading: false, tagline: response.tagline, relationship_status: response.relationship_status });
-            } else {
-                this.setState({ loading: false, tagline: '', relationship_status: '' });
-            }           
-        });
-
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        // chrome.runtime.sendMessage({ requestId: "EditStatus", params: { id: "1" }, payload: { name: "NewName" } }, response => {
-        //     console.log(response);
-        //   });
-
-        // alert('You info has been updated');
-        event.preventDefault();
-    }
-
-    render() {
-
-        if (this.state.loading) {
-            return (<div>Loading...</div>);
+    chrome.runtime.sendMessage(
+      { requestId: "GetProfile", params: { id: userId } },
+      response => {
+        console.log("RESPONSE: " + response);
+        if (response) {
+          this.setState({
+            loading: false,
+            tagline: response.tagline,
+            relationship_status: response.relationship_status,
+            profileExists: true,
+            userId,
+            name
+          });
         } else {
-            return (<div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Tagline:
-                        <input type="text" name="tagline" value={this.state.tagline} onChange={this.handleChange} />
-                    </label>
-    
-                    <label>
-                        Relationship Status:
-                        <input type="text" name="relationship_status" value={this.state.relationship_status} onChange={this.handleChange} />
-                    </label>      
-                    <input type="submit" value="Submit" />                  
-                </form>
-            </div>);
-        }        
+          this.setState({
+            loading: false,
+            tagline: "",
+            relationship_status: "",
+            profileExists: false,
+            userId,
+            name
+          });
+        }
+      }
+    );
+  }
+
+  handleRelationshipStatusChange = event => {
+    this.setState({ relationship_status: event.target.value });
+  };
+  handleTaglineChange = event => {
+    this.setState({ tagline: event.target.value });
+  };
+
+  handleSubmit = event => {
+    const { name, tagline, relationship_status, userId } = this.state;
+    chrome.runtime.sendMessage(
+      {
+        requestId: "EditStatus",
+        params: { id: userId },
+        payload: { name, tagline, relationship_status, id: userId }
+      },
+      response => {
+        console.log(response);
+      }
+    );
+
+    // alert('You info has been updated');
+    event.preventDefault();
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Tagline:
+              <input
+                type="text"
+                name="tagline"
+                value={this.state.tagline}
+                onChange={this.handleTaglineChange}
+              />
+            </label>
+
+            <label>
+              Relationship Status:
+              <input
+                type="text"
+                name="relationship_status"
+                value={this.state.relationship_status}
+                onChange={this.handleRelationshipStatusChange}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      );
     }
+  }
 }
